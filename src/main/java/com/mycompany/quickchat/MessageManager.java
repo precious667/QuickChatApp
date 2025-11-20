2091d7ee5296a7316aa4f14a3462fc0101e92eb6
precious667 <>
2025-11-20 18:27:24 +0000

    Part 3 commit 2

--- src/main/java/com/mycompany/quickchat/MessageManager.java
+++ src/main/java/com/mycompany/quickchat/MessageManager.java
@@ -7,7 +7,7 @@ public class MessageManager {
     private List<Message> sentMessages = new ArrayList<>();
     private List<Message> storedMessages = new ArrayList<>();
     private List<Message> disregardedMessages = new ArrayList<>();
-    
+    //array methods
         public String displayAllSentMessages() {
         if (sentMessages.isEmpty()) {
             return "No sent messages found.";
